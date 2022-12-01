7c156f64891cd5a6327ab1eb7d80b2fca367c17a
Mak Maid Mulabegovic <mulabegovicmak075@gmail.com>
2022-12-01 14:45:54 +0100

    api se moze pozvat 100 puta svaka 24 sata pa sad ne moze da radi tek sutra

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        coinManager.delegate = self
+        currencyPicker.dataSource = self // ? ne znam sta ovo znaci
+        currencyPicker.delegate = self
     }
-
-
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // koliko hoces da ih izaberes
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count // broj valuta koliko imas
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row] // vrati ti value elementa tog arraya
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { // akcija za taj element
+        let rowName = coinManager.currencyArray[row]
+        coinManager.fetchCurrency(currencyName: rowName)
+        self.currencyLabel.text = rowName
+    }
 }
 
+extension ViewController: CoinManagerDelegate {
+    
+    func didUpdateCoin(_ coinManager: CoinManager, coinValue: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = coinValue
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,64 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateCoin(_ coinManager: CoinManager, coinValue: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
+    let apiKey = "32B3D2B4-4189-4D59-8429-EDF09CDF3008"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    var delegate: CoinManagerDelegate?
+
+    func fetchCurrency(currencyName: String) {
+        let urlString = "\(baseURL)/\(currencyName)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlStringPar: String) {
+            
+            //1. Create a URL
+            if let url = URL(string: urlStringPar) {
+                //2. Create URL session
+                let session = URLSession(configuration: .default)
+                //3. Give the session a task
+                let task = session.dataTask(with: url) { (data, response, error) in
+    
+                    if error != nil {
+                        self.delegate?.didFailWithError(error: error!)
+                        return
+                    }
+                    
+                    if let safeData = data {
+                        if let bitcoinValue = self.parseJSON(safeData) {
+                            self.delegate?.didUpdateCoin(self, coinValue: bitcoinValue)
+                        }
+                    }
+                }
+                //4.Start the task
+                task.resume()
+            }
+        }
+    
+    
+    func parseJSON(_ coinData: Data) -> String? {
+            let decoder = JSONDecoder()
+            do {
+                let decodedData = try decoder.decode(CoinData.self, from: coinData)
+                let rateArg = decodedData.rate
+                
+                var rateString: String {
+                    return String(format: "%.2f", rateArg)
+                }
+                return rateString // vrati vrijednost neku npr 17083.34
+                
+            } catch {
+                delegate?.didFailWithError(error: error)
+                return nil
+            }
+        }
 }
